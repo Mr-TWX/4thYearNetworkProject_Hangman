05281af02e86c669828ac73f3182043c96beea7e
gina2014 <georginasheehan@gmail.com>
2015-11-25 23:02:04 +0000

    Added Signals

---
+++ Resources/Hangman.h
@@ -0,0 +1,6 @@
+#ifndef HANGMAN_H
+#define HANGMAN_H
+
+void play_hangman(int in, int out);
+
+#endif //HANGMAN_H
--- Resources/hangserver_iter.c
+++ Resources/hangserver_iter.c
@@ -8,21 +8,30 @@
  #include <syslog.h>
  #include <signal.h>
  #include <errno.h>
+ #include <sys/wait.h>
+ #include "Practical.h"
+ #include "Hangman.h"
 
- extern time_t time ();
+ 	void
+sig_chld(int sigNumber)
+{
+	pid_t	pid;
+	int		stat;
 
- int maxlives = 12;
- char *word [] = {
- # include "words"
- };
- # define NUM_OF_WORDS (sizeof (word) / sizeof (word [0]))
- # define MAXLEN 80 /* Maximum size in the world of Any string */
- # define HANGMAN_TCP_PORT 1066
+	while ( (pid = waitpid(-1, &stat, WNOHANG)) > 0) {
+		 printf("child %d terminated\n", pid); 
+	}
+	return;
+}
 
- main ()
+ int main ()
  {
- 	int sock, fd, client_len, pid;
+ 	int sock, fd, client_len, pidCount;
  	struct sockaddr_in server, client;
+ 	pid_t pid;
+ 	pidCount = 0;
+
+
 	 
 
  	sock = socket (AF_INET, SOCK_STREAM, 0);//0 or IPPROTO_TCP
@@ -30,6 +39,8 @@
  		perror ("creating stream socket");
  		exit (1);
  	}
+
+
 
  	server.sin_family = AF_INET;
  	server.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -42,84 +53,42 @@
 
  	listen (sock, 5);
 
- 	while (1) {
+ 	signal(SIGCHLD, sig_chld);
+
+ 	for( ; ;) {
+
  		client_len = sizeof (client);
- 		if ((fd = accept (sock, (struct sockaddr *) &client, &client_len)) <0) {
+ 		if ((fd = accept (sock, (struct sockaddr *) &client, &client_len)) < 0) {
+ 			if(errno == EINTR)
+ 			{
+ 				continue;
+ 			}
+ 			else
+ 			{
  			perror ("accepting connection");
  			exit (3);
+ 			}
+ 			
  		}
 		
+		pid = fork();
 		/*Fork Implementation*/
-		if(pid = fork() == 0){
-		  srand ((int) time ((long *) 0)); /* randomize the seed */
+		if (pid == 0){
+		  printf("pid value after fork is %d \n", pid);
+		  srand ((int) time ((long *) 0));
 		  close(sock);
 		  play_hangman (fd, fd);
-		}
-		else if (pid > 0){
-		  close(fd);
+		  exit(0);
  		}
-		else{
- 		close (fd);
+ 		else if (pid < 0)
+ 		{
+ 			perror( "Fork Failed" );
+ 		}
+
+ 		close(fd);
+ 		
 		}
- 	}
+ 
  }
 
- /* ---------------- Play_hangman () ---------------------*/
 
- play_hangman (int in, int out)
- {
- 	char * whole_word, part_word [MAXLEN],
- 	guess[MAXLEN], outbuf [MAXLEN];
-
- 	int lives = maxlives;
- 	int game_state = 'I';//I = Incomplete
- 	int i, good_guess, word_length;
- 	char hostname[MAXLEN];
-
- 	gethostname (hostname, MAXLEN);
- 	sprintf(outbuf, "Playing hangman on host% s: \n \n", hostname);
- 	write(out, outbuf, strlen (outbuf));
-
- 	/* Pick a word at random from the list */
- 	whole_word = word[rand() % NUM_OF_WORDS];
- 	word_length = strlen(whole_word);
- 	syslog (LOG_USER | LOG_INFO, "server chose hangman word %s", whole_word);
-
- 	// tell me what is going on
- 	printf("Word chosen is %s\n", whole_word);
-
- 	/* No letters are guessed Initially */
- 	for (i = 0; i <word_length; i++)
- 		part_word[i]='-';
- 	
-	part_word[i] = '\0';
-
- 	sprintf (outbuf, "%s %d \n", part_word, lives);
- 	write (out, outbuf, strlen(outbuf));
-
- 	while (game_state == 'I')
- 	/* Get a letter from player guess */
- 	{
-		while (read (in, guess, MAXLEN) <0) {
- 			if (errno != EINTR)
- 				exit (4);
- 			printf ("re-read the startin \n");
- 			} /* Re-start read () if interrupted by signal */
- 	good_guess = 0;
- 	for (i = 0; i <word_length; i++) {
- 		if (guess [0] == whole_word [i]) {
- 		good_guess = 1;
- 		part_word [i] = whole_word [i];
- 		}
- 	}
- 	if (! good_guess) lives--;
- 	if (strcmp (whole_word, part_word) == 0)
- 		game_state = 'W'; /* W ==> User Won */
- 	else if (lives == 0) {
- 		game_state = 'L'; /* L ==> User Lost */
- 		strcpy (part_word, whole_word); /* User Show the word */
- 	}
- 	sprintf (outbuf, "%s %d \n", part_word, lives);
- 	write (out, outbuf, strlen (outbuf));
- 	}
- }
